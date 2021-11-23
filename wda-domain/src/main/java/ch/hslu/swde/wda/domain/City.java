cc56c247d10817c0f1155610d33c3c9990e7944a
Finn Morgenthaler <finn.morgenthaler@stud.hslu.ch>
2021-11-23 12:09:12 +0100

    Added attributes and get/set to City

--- wda-domain/src/main/java/ch/hslu/swde/wda/domain/City.java
+++ wda-domain/src/main/java/ch/hslu/swde/wda/domain/City.java
@@ -2,4 +2,34 @@ package ch.hslu.swde.wda.domain;
 
 public class City {
 
+	
+	private int ZIPCode;
+	private String Name;
+	private float longitude;
+	private float latitude;
+	
+	
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public float getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(float longitude) {
+		this.longitude = longitude;
+	}
+	public float getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(float latitude) {
+		this.latitude = latitude;
+	}
+	public int getZIPCode() {
+		return ZIPCode;
+	}
+	
+	
 }
