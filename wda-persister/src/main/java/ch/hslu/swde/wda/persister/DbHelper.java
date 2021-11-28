8d76e0f6130c96746973692699752ea85c685105
Finn Morgenthaler <finn.morgenthaler@stud.hslu.ch>
2021-11-28 18:09:07 +0100

    2 select methods implemented, SelectAllCities and SelectCity

--- wda-persister/src/main/java/ch/hslu/swde/wda/persister/DbHelper.java
+++ wda-persister/src/main/java/ch/hslu/swde/wda/persister/DbHelper.java
@@ -1,33 +1,62 @@
 package ch.hslu.swde.wda.persister;
 
 import java.util.List;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import ch.hslu.swde.wda.domain.City;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 
 public class DbHelper {
 
-	
-	public static void selectCity(String name) {
+	private static final Logger Log = LogManager.getLogger(DbHelper.class);
+
+	/**
+	 * This Query returns all Cities, for which weather data is available - Query
+	 * A01
+	 * 
+	 * @return List of all Cities available in table "city" as City-Object
+	 */
+	public static List<City> selectAllCities() {
 
 		EntityManager em = JpaUtil.createEntityManager();
 
-		em.getTransaction( ).begin( );
-		TypedQuery<City> tQry = em.createQuery("SELECT c FROM City c where c.name= :name", City.class);
-		tQry.setParameter("name", name);
+		em.getTransaction().begin();
+		TypedQuery<City> tQry = em.createQuery("SELECT c FROM City c", City.class);
 
-		/* Alle Adresse-Entities aus der DB holen */
+		/* Get all City-Entities from DB */
 		List<City> cityFromDb = tQry.getResultList();
 
-	
 		em.close();
-		
-		for (City c : cityFromDb) {
-			System.out.println(c.toString());
-		}
-	}
-	
 
+		for (City c : cityFromDb) {
+			Log.info(c.toString());
+		}
+
+		return cityFromDb;
+	}
+
+	/**
+	 * This Query returns a single City based on the Name of a City specified as 
+	 * @param name - Cityname
+	 * @return a single City-Entity matching with the where-condition
+	 */
+	public static City selectCity(String name) {
+
+		EntityManager em = JpaUtil.createEntityManager();
+
+		em.getTransaction().begin();
+		TypedQuery<City> tQry = em.createQuery("SELECT c FROM City c where c.name = :name", City.class);
+		tQry.setParameter("name", name);
+
+		/* Get the single City-Entity from DB (matched by WHERE-Clause */
+		City city = tQry.getSingleResult();
+
+		em.close();
+
+		Log.info(city.toString());
+
+		return city;
+	}
 
 }
