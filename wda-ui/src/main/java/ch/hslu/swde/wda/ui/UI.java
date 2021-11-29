a152706a73ba95cbe0c70de94d182c3c333b17d4
Cyrill Küttel <cyrill.kuettel@stud.hslu.ch>
2021-11-29 21:46:38 +0100

    Add more tests

--- wda-ui/src/main/java/ch/hslu/swde/wda/ui/UI.java
+++ wda-ui/src/main/java/ch/hslu/swde/wda/ui/UI.java
@@ -3,10 +3,13 @@ package ch.hslu.swde.wda.ui;
 
 import ch.hslu.swde.wda.CheckConnection.Utils;
 import ch.hslu.swde.wda.domain.City;
+import ch.hslu.swde.wda.domain.WeatherData;
 import ch.hslu.swde.wda.persister.DbHelper;
+import ch.hslu.swde.wda.persister.PersistCity;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.sql.Date;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -33,20 +36,25 @@ public final class UI {
     private static final String CITY_URL = "http://swde.el.eee.intern:8080/weatherdata-provider/rest/weatherdata" +
             "/cities";
     private static final String SELECT_TIMESPAN_END = "Enddatum: ";
+    static final String DEFAULT_DATE_KEYWORD = "skip";
     private final String SHOW_CITY_MENU;
     private static final String PROMPT_USER_TO_LOGIN = "Bitte Loggen Sie sich ein.";
     private static final String ASK_PASSWORD = "Bitte Passwort eingeben: ";
     private static final String ASK_USERNAME = "Bitte Benutzername eingeben: ";
 
-    private static final String CONFIRM_NEW_USER_CREATED = "Der neue Benutzer wurde erstellt";
+    private static final String CONFIRM_NEW_USER_CREATED = "Erstellung erfolgreich.";
     private static final String WELCOME_MENU = " Einloggen [1]      Neuer Benutzer erstellen [2] ";
-    private static final String SELECT_TIMESPAN_START = "Startdatum angeben. Erwartetes Format ist \"dd.MM.yyyy\" \nEnter drücken für das default Datum.";
+    private static final String SELECT_TIMESPAN_START =
+            "Startdatum angeben. Erwartetes Format ist \"dd.MM.yyyy\" \n\"" + DEFAULT_DATE_KEYWORD + "\" für das " +
+                    "Standart " +
+                    "Datum";
     private static final String TIMESPAN = "Zeitspanne festlegen [1]        gesamter Zeitraum [2]          " +
             "Beenden [0]";
 
-    private static final String SELECT_DATA_OR_ALL_MENU = "Wetterdaten von einer Ortschaft laden [1]     " + "Wetterdaten über " +
-            "alle " + "Ortschaften laden [2] " + "     " +
-            "Beenden " + "[0]";
+    private static final String SELECT_DATA_OR_ALL_MENU =
+            "Wetterdaten von einer Ortschaft laden [1]     " + "Wetterdaten über " +
+                    "alle " + "Ortschaften laden [2] " + "     " +
+                    "Beenden " + "[0]";
 
     private String currentMenu;
     private static final String WARN_INVALID_LOGIN = "Ist nicht eine korrekte Benutzername / Password Kombination. " +
@@ -55,20 +63,22 @@ public final class UI {
             "Passwort muss mindestens 8 Zeichen lang sein, Benutzername mindestens 3 Zeichen";
 
     final static String DATE_FORMAT = "dd-MM-yyyy";
-    private static final String[] DEFAULT_DATE = {"27-11-2020", "30-11-2020"};
-    private static int DEFAULT_DATE_STATE = -1;
+    private static final String[] DEFAULT_DATE = {"29-11-2021", "30-11-2021"};
 
-    private static final String MIN_DATE_VALUE = "01-01-2020"; // minimum and maximum legal value, inclusive
-    private static final String MAX_DATE_VALUE = "31-12-2020";
+    // minimum and maximum valid value, inclusive
+    static final String MIN_DATE_VALUE = "01-01-2020";
+    static final String MAX_DATE_VALUE = "31-12-2020";
     static final int MAX_USERNAME_LEN = 40;
     static final int MIN_LENGTH_USERNAME = 3;
     static final int MIN_PASSWORD_LENGTH = 8;
+    Scanner sc;
 
     public UI() {
-        if (Utils.pingURL(CITY_URL, 100000)) {
-            Log.info("Connection established");
+        this.sc = new Scanner(System.in);
+        if (Utils.pingURL(CITY_URL, 10000)) {
+            Log.info("VPN connected!");
         } else {
-            Log.error("Could not ping swde.el.ee.intern:80. Are you connected to VPN?");
+            Log.error("Could not ping swde.el.ee.intern:80. Are you connected to https://vpn.hslu.ch?");
 
         }
 
@@ -81,15 +91,13 @@ public final class UI {
     }
 
 
-
     /**
      * starts a new User Interaction.
      */
     public void startFromBeginning() {
         int selectedOption;
-        String selectedCity = "";
-        String[] timePeriod = new String[2]; // index [0] ->  startDate
-                                             // index [1] ->  endDate
+        String selectedCity;
+        String[] selectedTimePeriod = new String[2]; // timePeriod[0] = startDate, timePeriod[1] = endDate
 
 
         currentMenu = WELCOME_MENU;
@@ -104,21 +112,17 @@ public final class UI {
             // TODO:  write new User to Database
         }
 
-        System.out.println(PROMPT_USER_TO_LOGIN);
-
         String[] creds;
         int totalLoginAttempts = 0;
         do {
             creds = askForUsernamePassword(totalLoginAttempts);
             totalLoginAttempts++;
-        } while(!isValidLogin(creds));
+        } while (!isValidLogin(creds));
 
-        if(isValidLogin(creds))  {
+        if (isValidLogin(creds)) {
             System.out.println("Erfolgreich Eingeloggt!");
             currentMenu = SELECT_DATA_OR_ALL_MENU;
         }
-
-
 
 
         showActiveMenu();
@@ -129,10 +133,10 @@ public final class UI {
             showActiveMenu();
             selectedOption = readOptionFromUser();
             selectedCity = cities[selectedOption];
-            timePeriod = getTimePeriod();
+            selectedTimePeriod = getTimePeriod();
 
         } else if (selectedOption == 2) { /* All cities are considered */
-            timePeriod = getTimePeriod();
+            selectedTimePeriod = getTimePeriod();
 
         }
 
@@ -140,11 +144,13 @@ public final class UI {
 
     /**
      * Read user input: Numerical in all cases. The actual meaning of the number can be read from the currentMenu.
+     *
      * @return the action the user takes as a numerical value.
      */
     public int readOptionFromUser() {
 
-        Scanner sc = new Scanner(System.in);
+        // Scanner sc = new Scanner(System.in);
+
         int eingabe = -1;
         List<Integer> validMenuValues = new ArrayList<>();
 
@@ -165,7 +171,7 @@ public final class UI {
 
         do {
             try {
-                eingabe = sc.nextInt();
+                eingabe = Integer.parseInt(sc.next());
                 if (!validMenuValues.contains(eingabe)) {
                     showActiveMenu();
                 }
@@ -183,7 +189,9 @@ public final class UI {
 
     /**
      * Asks the user to specify a Timespan. If the user does not want to restric search,
-     * we simply select the largest possible value for the date interval.
+     * we simply select the largest possible value for the date interval in 2020.
+     * If the startDate is not older than endDate, this just swaps them.
+     *
      * @return Array with index 0 representing start, and index 1 representing the end date.
      */
     public String[] getTimePeriod() {
@@ -193,14 +201,21 @@ public final class UI {
         showActiveMenu();
         int selectedOption = readOptionFromUser();
         if (selectedOption == 1) {
+            Log.info("You have decided to set a custom timePeriod");
             currentMenu = SELECT_TIMESPAN_START;
             showActiveMenu();
             String startDate = tryToParseDate();
-            timeframe[0] = startDate;
-            currentMenu = SELECT_TIMESPAN_END;
-            showActiveMenu();
-            String endDate = tryToParseDate();
+            String endDate;
+            if (!startDate.equals(DEFAULT_DATE[0])) { // no default date
+                timeframe[0] = startDate;
+                currentMenu = SELECT_TIMESPAN_END;
+                showActiveMenu();
+                endDate = tryToParseDate();
+            } else {
+                endDate = DEFAULT_DATE[1];
+            }
             timeframe[1] = endDate;
+
         } else if (selectedOption == 2) { // No constraints. All cities / Of All time
             timeframe[0] = MIN_DATE_VALUE;
             timeframe[1] = MAX_DATE_VALUE;
@@ -211,34 +226,59 @@ public final class UI {
     /**
      * Try to parse a single Date.
      */
-    private String tryToParseDate() {
-        Scanner sc = new Scanner(System.in);
+    public String tryToParseDate() {
+
         String input = " ";
         String date = "";
 
-        do { // Loop until valid date
+        do {
             if (!input.isEmpty()) {
                 try {
-                    input = sc.nextLine();
-                    if (isValidDate(input)) {
-                        date = input;
-                    } else if (!input.isEmpty()){
-                        System.out.println("Anderes Datumformat erwartet!");
+                    input = sc.next();
+                    if (input.equals(DEFAULT_DATE_KEYWORD)) {
+                        System.out.printf("Benutze das Standard Datum %s bis %s%n", DEFAULT_DATE[0],
+                                          DEFAULT_DATE[1]);
+                        // if (isValidDate(DEFAULT_DATE[0])) {
+                        date = DEFAULT_DATE[0];
+                        // }
+
+                    } else {
+                        if (isValidDate(input)) {
+                            date = input;
+                        } else {
+                            System.out.println("Datumsformat nicht korrekt. Versuchen Sie es noch einmal");
+                            showActiveMenu();
+                        }
                     }
                 } catch (Exception e) {
                     /* Clear the current Buffer */
                     sc.nextLine();
                     showActiveMenu();
                 }
-            } else { // On Enter key: just use the default date
-                System.out.println("Benutze das Standard Datum.");
-                DEFAULT_DATE_STATE++;
-                date = DEFAULT_DATE[DEFAULT_DATE_STATE];
             }
         } while (Objects.equals(date, ""));
         return date;
     }
 
+    public List<String> dummyScanner() {
+        // Scanner sc = new Scanner(System.in);
+        String input = "";
+        List<String> parsedSTrings = new ArrayList<>();
+        int count = 0;
+        do {
+            count++;
+            try {
+                input = sc.next();
+            } catch (Exception e) {
+                /* Clear the current Buffer */
+                sc.next();
+                showActiveMenu();
+            }
+            parsedSTrings.add(input);
+        } while (sc.hasNext());
+        Log.info(String.format("Looped %d times", count));
+        return parsedSTrings;
+    }
 
 
     public void showActiveMenu() {
@@ -248,91 +288,134 @@ public final class UI {
     }
 
 
-
     /**
-     * Expects a Date as String,  for example "27.11.2020"
+     * Expects a Date as String. If the year is not specified, it will asssume 2020!
+     * Accepted Formats:
+     * "27.11.2020"
+     * "27-11-2020"
+     * 27.11.
+     * 26-11-
+     *
      * @return True if the the input String is a valid Date, according to the specificed DATA_FORMAT
      */
     public static boolean isValidDate(String date) {
-        if (date.contains(".")) {
+        if (date.contains(".")) {  // unify format
             date = date.replaceAll("\\.", "-");
         }
+        date = inferYearIfNotPresent(date);
+
+        if (!date.contains("2020")) {
+            Log.warn(String.format(" %s is is outside of the valid year range. Year should always be 2020", date));
+            return false;
+        }
+
         try {
             DateFormat df = new SimpleDateFormat(DATE_FORMAT);
             df.setLenient(false);
             df.parse(date);
+
             return true;
         } catch (ParseException e) {
+            Log.warn(String.format("%s is not a valid date.", date));
             return false;
         }
     }
 
+    /*
+     * adds the year 2020 if not present.
+     * Input should not contain dots.
+     * It is assumed that this method is only called inside isValidDate!
+     */
+    public static String inferYearIfNotPresent(final String date) {
+        final String deliminer = "\\-";
+
+        String[] dates = date.split(deliminer);
+        /*
+        System.out.println(Arrays.toString(dates));
+        System.out.println();
+        System.exit(0);
+        */
+
+        String modifiedDateWithYear = "";
+        if (dates.length == 2) {
+            Log.info(String.format("date format without year detected. Date= %s", date));
+            modifiedDateWithYear = date + "2020";
+            return modifiedDateWithYear;
+        }
+        return date;
+    }
+
     /**
      * Determine if login is successful.
+     *
      * @return true if the login is a correct username / password combination, false if not.
      */
     private boolean isValidLogin(String[] checkThisLogin) {
         // only for testing (this will change as soon as we can validate logins on
         // the server-side)
-        return checkThisLogin[0] != " " && checkThisLogin[1].length() > 0 &&  checkThisLogin[0].length() > 0 ;
+        return checkThisLogin[0] != " " && checkThisLogin[1].length() > 0 && checkThisLogin[0].length() > 0;
     }
 
     /**
      * Ask the user to type in username and password. This method can be used both in login and when creating a new
      * user. It will Loop indefinitely, until the condition in {@link #simpleLoginValidationPassed(String, String)
      * condition passes}
-     * Then for this data the user provides, do some simple client-side
-     * validation, to prevent sendingn malformed data to our server: The username and passsword should not exceed a
-     * certain length.
+     *
      * @return String Array containing the username [index: 0] and password. [index: 1]
      */
-   private String[] askForUsernamePassword(int attemptCount) {
+    public String[] askForUsernamePassword(int attemptCount) {
 
-
-
-       if (attemptCount > 0) {
-           System.out.print(WARN_INVALID_LOGIN);
-       }
-       String[] creds = new String[2];
-       Scanner sc = new Scanner(System.in);
-       do {
-           try {
-               System.out.println();
-               System.out.print(ASK_USERNAME);
-               String username = sc.nextLine();
-               System.out.print(ASK_PASSWORD);
-               String password = sc.nextLine();
-               Log.info("das passort" + password);
-               if ( simpleLoginValidationPassed(username, password)) {
-                   creds[0] = username;
-                   creds[1] = password;
-               } else {
-                   System.out.println(WARN_LOGIN_VALIDATION_NOT_PASSED);
-               }
-           } catch (Exception e) {
-               Log.error("Error while reading Username or Password in method askForUsernamePassword", e);
-               System.err.println("\n Fehler beim Lesen der Login Daten");
-           }
-       } while (creds[0] == null && creds[1] == null);
-       return creds;
-   }
+        if (attemptCount > 0) {
+            System.out.print(WARN_INVALID_LOGIN);
+        }
+        String[] credentials = new String[2];
+        // removed scanner
+        do {
+            try {
+                System.out.print(ASK_USERNAME);
+                String username = sc.next();
+                System.out.print(ASK_PASSWORD);
+                String password = sc.next();
+                if (simpleLoginValidationPassed(username, password)) {
+                    credentials[0] = username;
+                    credentials[1] = password;
+                } else {
+                    System.out.println(WARN_LOGIN_VALIDATION_NOT_PASSED);
+                }
+            } catch (Exception e) {
+                Log.error("Error while reading Username or Password in method askForUsernamePassword", e);
+                System.err.println("\n Fehler beim Lesen der Login Daten");
+            }
+        } while (credentials[0] == null && credentials[1] == null);
+        return credentials;
+    }
 
     /**
-     * This Function already checks if the username and password pass certain basic validation.
+     * This Function already checks if the username and password pass certain basic validations.
+     * This is not a security measure, it just prevents sending malformed data to our server: For example: the username
+     * cannot be an empty String.
+     *
      * @return true if the credidentals are valid, false if there is problem.
      */
     public boolean simpleLoginValidationPassed(String username, String password) {
-        if ( username.length() < MIN_LENGTH_USERNAME || username.length() > MAX_USERNAME_LEN) {
+        if (username.length() < MIN_LENGTH_USERNAME || username.length() > MAX_USERNAME_LEN) {
+
+            Log.info(String.format("Username Length should be at least %d and not longer than %d",
+                                   MIN_LENGTH_USERNAME,
+                                   MAX_USERNAME_LEN));
             return false;
         }
-
-        if (password.length() < MIN_PASSWORD_LENGTH) return false;
+        if (password.length() < MIN_PASSWORD_LENGTH) {
+            Log.info(String.format("The minimum Password length is %d ", MIN_PASSWORD_LENGTH));
+            return false;
+        }
         return true;
     }
 
     /**
      * For each city, write it's name and coresponding index.
      * We then dislay this String on the Cosole, so that a sinle city can be selected by user.
+     *
      * @param cities
      */
     public final String generateCityWithIndex(final String[] cities) {
@@ -351,11 +434,43 @@ public final class UI {
         String[] cities = new String[dbCities.size()];
         Iterator<City> it = dbCities.iterator();
         int count = 0;
-        while ( it.hasNext()) {
+        while (it.hasNext()) {
             cities[count] = it.next().getName();
         }
 
         return cities;
     }
+
+    /**
+     * Only for testing
+     * copied from ch/hslu/swde/wda/persister/test.java
+     */
+    public void insertTestCities() {
+        //int zipCode,String name, float longitude, float latitude
+        City city1 = new City(4900, "Langenthal", 3.0f, 22.11f);
+        City city2 = new City(4901, "Bern", 5.6f, 4.922f);
+        City city3 = new City(8000, "Zurich", 2.7f, 5.9343f);
+
+        List<City> cityList = new ArrayList();
+        cityList.add(city1);
+        cityList.add(city2);
+        cityList.add(city3);
+        PersistCity.insertCity(cityList);
+    }
+
+    public void selectWeatherByDateAndCity(String cityname, String start, String end) {
+
+        start = "2020-12-30";
+        end = "2021-11-28";
+
+        java.sql.Date startDate = java.sql.Date.valueOf(start);
+        java.sql.Date endDate = Date.valueOf(end);
+
+
+        List<WeatherData> requestedWeatherData = DbHelper.selectWeatherDataSingleCity("Langenthal", startDate,
+                                                                                      endDate);
+    }
+
+
 }
 
--- wda-ui/src/test/java/ch/hslu/swde/wda/ui/UITest.java
+++ wda-ui/src/test/java/ch/hslu/swde/wda/ui/UITest.java
@@ -3,18 +3,21 @@ package ch.hslu.swde.wda.ui;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 /**
  * Testcases fuer {@link ch.hslu.swde.wda.ui.UI}.
  */
 
-// create tests to check if date in valid timespan 01.01.2020
-    // create tests to check if user added
+// create tests to check if date in valid timespan 01.01.2020 ( integrate this in project afterwards)
+// create tests to check if user added
 class UITest {
 
     @Disabled
@@ -22,16 +25,16 @@ class UITest {
     void testLoadingCitiesFromDatabaseContainsAllKnownCities() {
 
         /* This could be a Subset of all the cities. */
-          final String[] minimalExpectedCities = {"Lausanne", "Geneva", "Nyon", "Biel", "Bern", "Thun", "Adelboden",
+        final String[] minimalExpectedCities = {"Lausanne", "Geneva", "Nyon", "Biel", "Bern", "Thun", "Adelboden",
                 "Interlaken",
                 "Grindelwald", "Lauterbrunnen", "Meiringen", "Brig", "Saas-Fee", "Zermatt", "Basel", "Solothurn", "Olten",
                 "Aarau", "Baden", "Lucerne", "Buchrain", "Zug", "Rotkreuz", "Engelberg", "Schwyz", "Altdorf", "Erstfeld",
                 "Andermatt", "Realp", "Bellinzona", "Locarno", "Airolo", "Chur", "Arosa", "Davos", "St. Moritz", "Zurich",
                 "Winterthur", "Frauenfeld", "St. Gallen"};
 
-            UI ui = new UI();
-            String[] actualCities = ui.getCitiesFromDatabase();
-            assertTrue(Arrays.asList(actualCities).containsAll(Arrays.asList(minimalExpectedCities)));
+        UI ui = new UI();
+        String[] actualCities = ui.getCitiesFromDatabase();
+        assertTrue(Arrays.asList(actualCities).containsAll(Arrays.asList(minimalExpectedCities)));
     }
 
     @Disabled
@@ -44,7 +47,7 @@ class UITest {
     }
 
     @Test
-    void testsimpleLoginValidationPassed_ShouldFailForEmptyStrings() {
+    void testLoginShouldRefuseEmptyStrings() {
         UI ui = new UI();
         String username = "";
         String password = "";
@@ -55,19 +58,124 @@ class UITest {
     void testMaximumUsernameLengthShouldNotExceedAValue() {
         UI ui = new UI();
         String username = "";
-        String password = "";
-        for (int i = 0; i < UI.MAX_USERNAME_LEN ; i++) {
+        String password = "asdfadsfdsfdsf";
+        for (int i = 0; i < UI.MAX_USERNAME_LEN * 2; i++) {
             username += "u";
         }
         assertThat(ui.simpleLoginValidationPassed(username, password)).isFalse();
     }
 
-     @Test
-     @Disabled
-     void testaskForUsernamePassword() {
-        // test the scanner in functionality
+    @Test
+    void testPasswordAtLeast8Characters() {
+        UI ui = new UI();
+        String username = "test";
+        String password = "pw";
+        assertThat(ui.simpleLoginValidationPassed(username, password)).isFalse();
+    }
 
-     }
+    @Test
+    void testGetTimePeriodForMaximumDateRange() {
+
+        InputStream sysInBackup = System.in; // backup System.in to restore it later
+        String[] expectedTimeFrame = {UI.MIN_DATE_VALUE, UI.MAX_DATE_VALUE};
+        ByteArrayInputStream in = new ByteArrayInputStream("2".getBytes()); // Option 2 means we use all dates
+        System.setIn(in);
+        UI ui = new UI();
+        // Test with System.in
+        String[] actualTimeFrame = ui.getTimePeriod();
+        assertThat(actualTimeFrame).isEqualTo(expectedTimeFrame);
+
+        // reset System.in to its original
+        System.setIn(sysInBackup);
+    }
+
+
+
+
+    @Test
+    void testDummyScannnerParsingMultipleInputs() {
+
+        InputStream sysInBackup = System.in; // backup System.in to restore it later
+        ByteArrayInputStream in = new ByteArrayInputStream("This Works now".getBytes());
+        System.setIn(in);
+        UI ui = new UI();
+
+        // Test with System.in
+        List<String> result = ui.dummyScanner(); // Observe that the scan stops at space
+        assertThat(result.get(0)).isEqualTo("This");
+        assertThat(result.get(1)).isEqualTo("Works");
+        assertThat(result.get(2)).isEqualTo("now");
+        System.out.println(result);
+
+        System.setIn(sysInBackup);
+    }
+
+    @Test
+    void testTryToParseDate_usingvalidDate() {
+        String validDate = "10.01.2020";
+        InputStream sysInBackup = System.in; // backup System.in to restore it later
+        ByteArrayInputStream in = new ByteArrayInputStream(validDate.getBytes());
+        System.setIn(in);
+        UI ui = new UI();
+
+        String date = ui.tryToParseDate();
+        assertEquals(date, validDate);
+        // reset System.in to its original
+        System.setIn(sysInBackup);
+
+    }
+
+    @Test
+    void testTryToParseDate_usingDateWithoutYear() {
+        String dateWithNoYearSpecified = "10.11."; // actually, it's always 2020
+        assertTrue(UI.isValidDate(dateWithNoYearSpecified));
+
+    }
+
+    @Test
+    void testTryToParseDate_usingDateWithWrongYear() { // test what happens when the year is not 2020
+        String dateWithNoYearSpecified = "27.11.1999";
+        assertFalse(UI.isValidDate(dateWithNoYearSpecified));
+
+    }
+
+    @Test
+    void testinferYearIfNotPresent() {
+
+        String dateWithoutYear = "10-11-";
+        // actually, year always 2020. It should infer year = 2020 if no year is specified
+        dateWithoutYear = UI.inferYearIfNotPresent(dateWithoutYear);
+        assertThat(dateWithoutYear).isEqualTo("10-11-2020");
+
+
+    }
+
+    @Test
+    void testTryToParseDate_usingWeirdDateInput() {
+        String Not_A_Date = "1-1-1";
+        assertFalse(UI.isValidDate(Not_A_Date));
+    }
+
+    @Test
+    void testTryToParseDate_usingWeirdDateInput2() {
+        String completlyIncorrectDate = "10.11"; // expect everything!
+        assertFalse(UI.isValidDate(completlyIncorrectDate));
+    }
+
+
+    @Test
+    void testaskForUsernamePassword() {
+        String[] newUserCredentials = {"username", "ultimate_secret_password"};
+        InputStream sysInBackup = System.in; // backup System.in to restore it later
+        ByteArrayInputStream in = new ByteArrayInputStream("username ultimate_secret_password".getBytes());
+        System.setIn(in);
+
+        UI ui = new UI();
+        String[] credentials = ui.askForUsernamePassword(0);
+        assertThat(credentials).isEqualTo(newUserCredentials);
+        // reset System.in to its original
+        System.setIn(sysInBackup);
+    }
 
 
     @Test
